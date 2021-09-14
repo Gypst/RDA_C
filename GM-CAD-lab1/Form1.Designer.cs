8d7702a99f0d6298d3b1a96360d3a2acf136c98b
Gypst <43269239+Gypst@users.noreply.github.com>
2021-09-14 14:12:19 +0700

    Геометрическое моделирование САПР
    Лаб 1

---
+++ GM-CAD-lab1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace GM_CAD_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoomToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotateLeftToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotateRightToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.verticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.zoomToolStripMenuItem,
+            this.zoomToolStripMenuItem1,
+            this.rotateLeftToolStripMenuItem1,
+            this.rotateRightToolStripMenuItem2,
+            this.verticalToolStripMenuItem,
+            this.horizontalToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(494, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.saveToolStripMenuItem1});
+            this.openToolStripMenuItem.Image = global::GM_CAD_lab1.Properties.Resources.icons8_файл_30;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.openToolStripMenuItem.Text = "File";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Image = global::GM_CAD_lab1.Properties.Resources.icons8_добавить_файл_30;
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Image = global::GM_CAD_lab1.Properties.Resources.icons8_сохранить_30;
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // zoomToolStripMenuItem
+            // 
+            this.zoomToolStripMenuItem.Image = global::GM_CAD_lab1.Properties.Resources.icons8_уменьшить_30;
+            this.zoomToolStripMenuItem.Name = "zoomToolStripMenuItem";
+            this.zoomToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.zoomToolStripMenuItem.Text = "Zoom-";
+            this.zoomToolStripMenuItem.Click += new System.EventHandler(this.zoomToolStripMenuItem_Click);
+            // 
+            // zoomToolStripMenuItem1
+            // 
+            this.zoomToolStripMenuItem1.Image = global::GM_CAD_lab1.Properties.Resources.icons8_приблизить_30;
+            this.zoomToolStripMenuItem1.Name = "zoomToolStripMenuItem1";
+            this.zoomToolStripMenuItem1.Size = new System.Drawing.Size(75, 20);
+            this.zoomToolStripMenuItem1.Text = "Zoom+";
+            this.zoomToolStripMenuItem1.Click += new System.EventHandler(this.zoomToolStripMenuItem1_Click);
+            // 
+            // rotateLeftToolStripMenuItem1
+            // 
+            this.rotateLeftToolStripMenuItem1.Image = global::GM_CAD_lab1.Properties.Resources.icons8_повернуть_страницу_против_часовой_стрелки_30;
+            this.rotateLeftToolStripMenuItem1.Name = "rotateLeftToolStripMenuItem1";
+            this.rotateLeftToolStripMenuItem1.Size = new System.Drawing.Size(57, 20);
+            this.rotateLeftToolStripMenuItem1.Text = "-90°";
+            this.rotateLeftToolStripMenuItem1.Click += new System.EventHandler(this.rotateLeftToolStripMenuItem1_Click);
+            // 
+            // rotateRightToolStripMenuItem2
+            // 
+            this.rotateRightToolStripMenuItem2.Image = global::GM_CAD_lab1.Properties.Resources.icons8_повернуть_страницу_по_часовой_стрелке_30;
+            this.rotateRightToolStripMenuItem2.Name = "rotateRightToolStripMenuItem2";
+            this.rotateRightToolStripMenuItem2.Size = new System.Drawing.Size(60, 20);
+            this.rotateRightToolStripMenuItem2.Text = "+90°";
+            this.rotateRightToolStripMenuItem2.Click += new System.EventHandler(this.rotateRightToolStripMenuItem2_Click);
+            // 
+            // verticalToolStripMenuItem
+            // 
+            this.verticalToolStripMenuItem.Image = global::GM_CAD_lab1.Properties.Resources.icons8_отразить_по_вертикали_30;
+            this.verticalToolStripMenuItem.Name = "verticalToolStripMenuItem";
+            this.verticalToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.verticalToolStripMenuItem.Text = "Vertical";
+            this.verticalToolStripMenuItem.Click += new System.EventHandler(this.verticalToolStripMenuItem_Click);
+            // 
+            // horizontalToolStripMenuItem
+            // 
+            this.horizontalToolStripMenuItem.Image = global::GM_CAD_lab1.Properties.Resources.icons8_отразить_по_горизонтали_30;
+            this.horizontalToolStripMenuItem.Name = "horizontalToolStripMenuItem";
+            this.horizontalToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.horizontalToolStripMenuItem.Text = "Horizontal";
+            this.horizontalToolStripMenuItem.Click += new System.EventHandler(this.horizontalToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(494, 326);
+            this.panel1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(494, 326);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(510, 365);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoomToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rotateLeftToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rotateRightToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem verticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horizontalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GM-CAD-lab1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GM_CAD_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap1 = new Bitmap(pictureBox1.Image);
+            if (bitmap1 != null)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate180FlipY);
+                pictureBox1.Image = bitmap1;
+            }
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap1 = new Bitmap(pictureBox1.Image);
+            if (bitmap1 != null)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate180FlipX);
+                pictureBox1.Image = bitmap1;
+            }
+        }
+
+        private void rotateLeftToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap1 = new Bitmap(pictureBox1.Image);
+            if (bitmap1 != null)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = bitmap1;
+            }
+        }
+
+        private void rotateRightToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap1 = new Bitmap(pictureBox1.Image);
+            if (bitmap1 != null)
+            {
+                bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                pictureBox1.Image = bitmap1;
+            }
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // Wrap the creation of the OpenFileDialog instance in a using statement,
+            // rather than manually calling the Dispose method to ensure proper disposal
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Image Files(*.BMP;*.JPG;*.PNG)|*.BMP;*.JPG;*.PNG";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    PictureBox PictureBox1 = new PictureBox();
+
+                    // Create a new Bitmap object from the picture file on disk,
+                    // and assign that to the PictureBox.Image property
+                    pictureBox1.Image = new Bitmap(dlg.FileName);
+
+                    // Add the new control to its parent's controls collection
+                    this.Controls.Add(PictureBox1);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image != null)
+                {
+                    pictureBox1.Image.Save("myBitmap.bmp");
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("There was a problem saving the file." +
+                    "Check the file permissions.");
+            }
+        }
+
+        private void zoomToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.Image = ZoomPicture(pictureBox1.Image, new Size((pictureBox1.Width * 2),
+                                                                        (pictureBox1.Height * 2)));
+            pictureBox1.Width = Convert.ToInt32(pictureBox1.Width * 2);
+            pictureBox1.Height = Convert.ToInt32(pictureBox1.Height * 2);
+            //pictureBox1.Width = (int)(pictureBox1.Width * 2);
+            //pictureBox1.Height = (int)(pictureBox1.Height * 2);
+        }
+
+        private void zoomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            pictureBox1.Image = ZoomPicture(pictureBox1.Image, new Size((pictureBox1.Width / 2),
+                                                                        (pictureBox1.Height / 2)));
+            pictureBox1.Width = Convert.ToInt32(pictureBox1.Width / 2); 
+            pictureBox1.Height = Convert.ToInt32(pictureBox1.Height / 2); 
+            //pictureBox1.Width = (int)(pictureBox1.Width / 2);
+            //pictureBox1.Height = (int)(pictureBox1.Height / 2);
+        }
+
+        Image ZoomPicture(Image img, Size size)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(size.Width),
+                                    Convert.ToInt32(size.Height));
+            Graphics gpu = Graphics.FromImage(bm);
+            gpu.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bm;
+        }
+
+    }
+}
